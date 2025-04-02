eea374d41b987ceb742af8298b88271a8c892d70
Kuronami <daomanhducanh@gmail.com>
2025-04-02 11:19:18 +0700

    first

---
+++ src/main/java/com/DMDA/Lab09_10/repository/EmployeeRespository.java
@@ -0,0 +1,9 @@
+package com.DMDA.Lab09_10.repository;
+
+import com.DMDA.Lab09_10.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/test/java/com/DMDA/Lab09_10/Lab0910ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DMDA.Lab09_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0910ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
